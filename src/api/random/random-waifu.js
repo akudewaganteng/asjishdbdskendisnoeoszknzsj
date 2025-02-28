af6e2d5ad87bf251348dfa8004a12ae86ffa8c86
akudewaganteng <yvinn435@gmail.com>
2025-03-01 00:10:37 +0700

    Update random-waifu.js

--- src/api/random/random-waifu.js
+++ src/api/random/random-waifu.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 module.exports = function(app) {
-    async function bluearchive() {
+    async function randomwaifu() {
         try {
             const { data } = await axios.get(`https://raw.githubusercontent.com/akudewaganteng/ahzzxxxx/refs/heads/main/links.json`)
             const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
@@ -11,7 +11,7 @@ module.exports = function(app) {
     }
     app.get('/random/waifu', async (req, res) => {
         try {
-            const pedo = await bluearchive();
+            const pedo = await randomwaifu();
             res.writeHead(200, {
                 'Content-Type': 'image/png',
                 'Content-Length': pedo.length,
