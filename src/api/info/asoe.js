2c33260e8c6129b4fb8e714dd49b743b395d6f6f
akudewaganteng <yvinn435@gmail.com>
2025-03-07 15:11:47 +0700

    Add files via upload

---
+++ src/api/info/asoe.js
@@ -0,0 +1,92 @@
+const JsConfuser = require('js-confuser');
+const config = require('../settings');
+
+let userNameForObfuscation = '';
+
+const setUserName = (name) => {
+  userNameForObfuscation = name; 
+};
+
+module.exports = function (app) {  
+  async function appolofree(sourceCode) {
+    try {
+      console.log("@luminelll Result:\n", sourceCode);
+
+      const obfuscatedCode = await JsConfuser.obfuscate(sourceCode, {
+        target: 'node',
+        hexadecimalNumbers: true,
+        identifierGenerator: function () {
+          const randomValue = Math.floor(Math.random() * 9000) + 1000;
+          console.log(randomValue);
+          
+          const repeatedChar = "气".repeat(1);
+          return userNameForObfuscation + repeatedChar + randomValue;     
+        },
+        preserveFunctionLength: true,
+        lock: {
+          antiDebug: true,
+          tamperProtection: true,
+          selfDefending: true,
+        },
+        variableMasking: {
+          value: true,
+          limit: 30,
+        },
+        astScrambler: true,
+        stringConcealing: true,
+        renameVariables: true,
+        renameGlobals: true,
+        renameLabels: true,
+        stringSplitting: {
+          value: true,
+          limit: 20,
+        },
+        compact: true,
+        stringCompression: true,
+      });
+
+      console.log("@luminelll Result Enc:\n", obfuscatedCode);
+      return obfuscatedCode;
+    } catch (error) {
+      console.error('Terjadi kesalahan saat obfuscation dengan jsconfuser:', error);
+      throw error;
+    }
+  }
+
+  app.get('/api/obfuscatedcustom', async (req, res) => { 
+    const { apikey, code, nama } = req.query;
+
+    if (!apikey) return res.json("Isi Parameter Apikey.");
+    if (!code) return res.json("Isi Parameter Code.");
+    if (!nama) return res.json("Isi Parameter Nama.");
+
+    const check = config.apikey;
+    if (!check.includes(apikey)) {
+      return res.json({
+        status: false,
+        result: "Apikey Tidak Valid!."
+      });
+    }
+
+    try {
+      await setUserName(nama);
+
+      const obfuscatedCode = await appolofree(code);
+
+      if (!obfuscatedCode) {
+        return res.json({
+          status: false,
+          result: "Obfuscation failed."
+        });
+      }
+
+      res.json({
+        status: true,
+        result: obfuscatedCode
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "An error occurred while processing your request." });
+    }
+  });
+};
